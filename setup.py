0599c80f239551f3b3174e55c13824d006549c56
Yigit Erkal <yerkal1998@gmail.com>
2022-03-09 11:05:26 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     # TODO: Write a globally unique name which will be listed on PyPI
-    name="yigit-simple-dictionary",
+    name="yigiterkall/cs453-hw1",
     author="Yigit Erkal",  # TODO: Write your name
     version="2.0.0",
     packages=["dictionary"],
     install_requires=[
-        "requests>=2.23.0",
+        "requests>=2.23.0", "dictionary",
     ],
     python_requires=">=3.8",
 
