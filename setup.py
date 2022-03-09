1f296a015d11838e600e1d46c6f2ad5c7e555142
Yigit Erkal <yerkal1998@gmail.com>
2022-03-09 21:06:02 +0300

    ..

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = pathlib.Path(__file__).parent.resolve()
 
 setup(
     # TODO: Write a globally unique name which will be listed on PyPI
-    name="yigiterkall/cs453-hw1",
+    name="yigiterkall-cs453-hw1",
     author="Yigit Erkal",  # TODO: Write your name
     version="2.0.0",
     description="basic",
@@ -15,6 +15,6 @@ setup(
         "requests>=2.23.0",
     ],
     python_requires=">=3.8",
-    
+
 
 )
